1c8985292c616790b345b70884037190b8774543
YXY8899 <yeoxinyu9@gmail.com>
2025-03-04 15:01:19 +0800

    feat: Created webpage for cat profiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar"; // Import Navbar
-import MenuBar from "./components/menubar.tsx"
+import MenuBar from "./components/menubar.tsx";
 import AboutLKP from "./pages/AboutLKP";
-import ResidentCats from "./pages/ResidentCats.tsx";
-import AdoptableCats from "./pages/AdoptableCats.tsx";
 import Donate from "./pages/Donate";
 import InKindDonations from "./pages/InKindDonations";
-import VolunteerOpportunities from "./pages/VolunteerOpportunities"
+import VolunteerOpportunities from "./pages/VolunteerOpportunities";
 import Events from "./pages/Events";
 import CatTherapy from "./pages/CatTherapy";
 import ForCatRescuers from "./pages/ForCatRescuers";
+import CatGallery from "./pages/CatGallery";
+import CatProfile from "./pages/CatProfile";
 
 const App = () => {
   return (
     <Router>
-      
-      <MenuBar/>
+      <MenuBar /> {/* Display the Navbar at the top */}
 
       <Routes>
         <Route path="/" element={<AboutLKP />} />
-        <Route path="/resident-cats" element={<ResidentCats />} />
-        <Route path="/adoptable-cats" element={<AdoptableCats />} />
+        <Route path="/cat-gallery" element={<CatGallery />} />
+        <Route path="/cats/:slug" element={<CatProfile />} />
         <Route path="/donate" element={<Donate />} />
         <Route path="/in-kind-donations" element={<InKindDonations />} />
         <Route path="/events" element={<Events />} />
         <Route path="/cat-therapy" element={<CatTherapy />} />
-        <Route path="/volunteer-opportunities" element={<VolunteerOpportunities />} />
         <Route path="/for-cat-rescuers" element={<ForCatRescuers />} />
+        <Route path="/volunteer-opportunities" element={<VolunteerOpportunities />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
+
+
--- src/components/menubar.tsx
+++ src/components/menubar.tsx
@@ -1,91 +1,85 @@
 import React from "react";
 import {
     Menubar,
-    MenubarCheckboxItem,
-    MenubarContent,
     MenubarItem,
     MenubarMenu,
-    MenubarRadioGroup,
-    MenubarRadioItem,
-    MenubarSeparator,
-    MenubarShortcut,
-    MenubarSub,
-    MenubarSubContent,
-    MenubarSubTrigger,
     MenubarTrigger,
+    MenubarContent,
 } from "@/components/ui/menubar";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./menubar.css";
 import profilePic from "../assets/LKPLogo.jpg";
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Chewy&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap');
-</style>
+
+// Import cats data from JSON file
+import cats from "../data/cats.json";
 
 const MenuBar = () => {
     return (
-    <Menubar className = "menubar"> 
-        <div className = "heading-logo">
-            <img src = {profilePic} alt="Logo" className="logo-img" />
-            <h1 className = "heading">Love Kuching Project</h1>
-        </div>
-        <div className = "menu-items">
-        <MenubarMenu>
-            <MenubarTrigger className = "text">
-                <Link to ="/">About LKP</Link>
-            </MenubarTrigger>
-        </MenubarMenu>
+        <Menubar className="menubar">
+            <div className="heading-logo">
+                <img src={profilePic} alt="Logo" className="logo-img" />
+                <h1 className="heading">Love Kuching Project</h1>
+            </div>
+            <div className="menu-items">
+                <MenubarMenu>
+                    <MenubarTrigger className="text">
+                        <Link to="/">About LKP</Link>
+                    </MenubarTrigger>
+                </MenubarMenu>
 
-        <MenubarMenu>
-            <MenubarTrigger className = "text">Our Cats</MenubarTrigger>
-            <MenubarContent>
-            <MenubarItem>
-                <Link to="/resident-cats">Resident Cats</Link>
-            </MenubarItem>
-            <MenubarItem>
-                <Link to="/adoptable-cats">Adoptable Cats</Link>
-            </MenubarItem>
-            </MenubarContent>
-        </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger className="text">
+                        <Link to="/cat-gallery">Our Cats</Link>
+                    </MenubarTrigger>
 
-      <MenubarMenu>
-        <MenubarTrigger className = "text">Support Us</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            <Link to="/donate">Donate</Link>
-          </MenubarItem>
-          <MenubarItem>
-            <Link to="/in-kind-donations">In-Kind Donations</Link>
-          </MenubarItem>
-          <MenubarItem>
-            <Link to="/events">Events</Link>
-          </MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger className = "text">
-            <Link to ="/cat-therapy">Cat Therapy</Link>
-        </MenubarTrigger>
-      </MenubarMenu>
+                    <MenubarContent>
+                        {/* Dynamically create menu items for each cat */}
+                        {cats.map((cat) => (
+                            <MenubarItem key={cat.slug}>
+                                <Link to={`/cats/${cat.slug}`}>{cat.name}</Link>
+                            </MenubarItem>
+                        ))}
+                    </MenubarContent>
+                </MenubarMenu>
 
-      <MenubarMenu>
-        <MenubarTrigger className = "text" >Join Us</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            <Link to="/volunteer-opportunities">Volunteer Opportunities</Link>
-          </MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger className="text">Support Us</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarItem>
+                            <Link to="/donate">Donate</Link>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <Link to="/in-kind-donations">In-Kind Donations</Link>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <Link to="/events">Events</Link>
+                        </MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
 
-      <MenubarMenu>
-        <MenubarTrigger className = "text">
-            <Link to ="/for-cat-rescuers">For Cat Rescuers</Link>
-        </MenubarTrigger>
-      </MenubarMenu>
-      </div>
-      
-        
-    </Menubar>
-  )
-}
+                <MenubarMenu>
+                    <MenubarTrigger className="text">
+                        <Link to="/cat-therapy">Cat Therapy</Link>
+                    </MenubarTrigger>
+                </MenubarMenu>
+
+                <MenubarMenu>
+                    <MenubarTrigger className="text">Join Us</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarItem>
+                            <Link to="/volunteer-opportunities">Volunteer Opportunities</Link>
+                        </MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+
+                <MenubarMenu>
+                    <MenubarTrigger className="text">
+                        <Link to="/for-cat-rescuers">For Cat Rescuers</Link>
+                    </MenubarTrigger>
+                </MenubarMenu>
+            </div>
+        </Menubar>
+    );
+};
 
 export default MenuBar;
