e8657b8ce08692c64d138e59e9c58bb4232f624f
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2025-04-01 00:37:41 +0800

    fix: change from react router to hash router for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import MenuBar from "./components/menubar.tsx";
 import Home from "./pages/Home";
 import AboutLKP from "./pages/AboutLKP";
