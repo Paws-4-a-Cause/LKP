8e93db656d7691e5a427124664b2173229b3abf8
Nayana Mahesh <nayanamahesh7@gmail.com>
2025-04-08 17:46:29 +0800

    Update to real info

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -7,14 +7,22 @@ import {
 } from "@/components/ui/carousel";
 import "./Events.css"; // Import the external CSS file
 import eventData from "../data/events.json";
-import event1Img from "../assets/events/event1_image.jpg";
-import event2Img from "../assets/events/event2_image.jpg";
-import event3Img from "../assets/events/event3_image.jpeg";
+import event1Img from "../assets/events/SMUPaw.jpg";
+import event2Img from "../assets/events/Brickland.jpg";
+import event3Img from "../assets/events/NanHuaTalk.jpg";
+import event4Img from "../assets/events/PurrsaMalam.jpg";
+import event5Img from "../assets/events/NeeSoonPets.png";
+import event6Img from "../assets/events/CashewPetsDay.png";
+import event7Img from "../assets/events/SingaporeCatFestival.jpg";
 
 const imageMap: { [key: string]: string } = {
-  "event1_image.jpg": event1Img,
-  "event2_image.jpg": event2Img,
-  "event3_image.jpeg": event3Img,
+  "SMUPaw.jpg": event1Img,
+  "Brickland.jpg": event2Img,
+  "NanHuaTalk.jpg": event3Img,
+  "PurrsaMalam.jpg": event4Img,
+  "NeeSoonPets.png": event5Img,
+  "CashewPetsDay.png": event6Img,
+  "SingaporeCatFestival.jpg": event7Img,
 };
 
 const Events = () => {
@@ -27,7 +35,7 @@ const Events = () => {
               <CarouselItem key={event.slug} className="carousel-item">
                 <div className="event-description">
                   <h1 className="event-heading">{event.name}</h1>
-                  <img className="event-image" src={imageMap[event.image]} alt={event.name} />
+                  <img className="event-image" src={imageMap[event.image]} />
                   <div className="event-details">
                     <p><strong>📅 Date:</strong> {event.date}</p>
                     <p><strong>🕒 Time:</strong> {event.time}</p>
@@ -41,7 +49,9 @@ const Events = () => {
                     </ul>
                     <p dangerouslySetInnerHTML={{ __html: event.cta }}></p>
                   </div>
-                  <a href="#" className="register-button">📩 Register here</a>
+                  <a href="https://www.instagram.com/luvkuching/?hl=en" className="register-button" target="_blank" rel="noopener noreferrer">
+                    📸 View Details on Instagram
+                  </a>
                 </div>
               </CarouselItem>
             ))}
